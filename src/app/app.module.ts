e6b0fce14529e926180bd4acc9a9d294e736bd92
charles bravo <charles bravo>
2018-05-04 21:21:45 +0200

    RUN apt-get npo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+import {HttpClientModule} from '@angular/common/http';
+
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {DataService} from './data.service';
+import {AppRoutingModule} from './app-routing.module';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
